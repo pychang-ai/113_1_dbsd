d3a63cdd19d9a9a6c2f6cb6bd2624d8c95848209
pyc <pychang@nkust.edu.tw>
2024-11-21 08:11:49 +0800

    update

--- ch06/ch6-3-2.php
+++ ch06/ch6-3-2.php
@@ -1,31 +1,38 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-<meta charset="utf-8" />
-<title>ch6-3-2.php</title>
-<?php 
-// 函數不使用靜態變數
-function nonStaticVar() {
-   $count = 0;  $count += 2;   // 區域變數加一
-   return $count;
-}
-// 函數使用靜態變數 */ 
-function staticVar() {
-   static $count = 0;  $count += 2; // 靜態變數加一
-   return $count;
-}
-?>
+   <meta charset="utf-8" />
+   <title>ch6-3-2.php</title>
+   <?php
+   // 函數不使用靜態變數
+   function nonStaticVar()
+   {
+      $count = 0;
+      $count += 2;   // 區域變數加一
+      return $count; // 回傳區域變數 $count
+   }
+   // 函數使用靜態變數 */ 
+   function staticVar()
+   {
+      static $count = 0;
+      $count += 2; // 靜態變數加一
+      return $count;
+   }
+   ?>
 </head>
+
 <body>
-<?php 
-$v1 = nonStaticVar();  // 函數呼叫
-$v2 = nonStaticVar();
-$v3 = nonStaticVar();
-print "不使用靜態變數: $v1, $v2, $v3<br/>";
-$v1 = staticVar();     // 函數呼叫
-$v2 = staticVar();
-$v3 = staticVar();
-print "使用靜態變數: $v1, $v2, $v3<br/>"; 
-?>
+   <?php
+   $v1 = nonStaticVar();  // 函數呼叫
+   $v2 = nonStaticVar();
+   $v3 = nonStaticVar();
+   print "不使用靜態變數: $v1, $v2, $v3<br/>";
+   $v1 = staticVar();     // 函數呼叫
+   $v2 = staticVar();
+   $v3 = staticVar();
+   print "使用靜態變數: $v1, $v2, $v3<br/>";
+   ?>
 </body>
+
 </html>
--- ch06/ch6-5.php
+++ ch06/ch6-5.php
@@ -1,24 +1,29 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-<meta charset="utf-8" />
-<title>ch6-5.php</title>
-<?php 
-include "ch6-5-1.inc";
-include_once("ch6-5-2.inc");
-?>
+    <meta charset="utf-8" />
+    <title>ch6-5.php</title>
+    <?php
+    // 引入 ch6-5-1.inc 檔案
+    include "ch6-5-1.inc";
+    // 引入 ch6-5-2.inc 檔案，只引入一次
+    include_once("ch6-5-2.inc");
+    ?>
 </head>
+
 <body>
-<?php
-require "ch6-5-1.inc"; 
-$amount = 1000;
-$rate = 28.1;
-// 呼叫函數
-$result = rateExchange($amount, $rate);
-print "1000美金兌換新台幣=". $result ."元<br/>";
-require ("ch6-5-1.inc"); 
-$file = "ch6-5-1.inc";
-require $file;
-?>
+    <?php
+    require "ch6-5-1.inc";
+    $amount = 1000;
+    $rate = 28.1;
+    // 呼叫函數
+    $result = rateExchange($amount, $rate);
+    print "1000美金兌換新台幣=" . $result . "元<br/>";
+    require("ch6-5-1.inc");
+    $file = "ch6-5-1.inc";
+    require $file;
+    ?>
 </body>
+
 </html>
