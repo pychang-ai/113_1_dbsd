1c84b0cdd88fba78ff9324582290d80616abbacd
pyc <pychang@nkust.edu.tw>
2024-12-25 23:45:08 +0800

    update aws conncection

--- ch12/ch12_sup/sup01_connect.php
+++ ch12/ch12_sup/sup01_connect.php
@@ -1,4 +1,5 @@
 <?php
+//for you XAMPP
 $servername = "localhost";
 $username = "root";
 $password = "";
@@ -7,9 +8,17 @@ $dbname = "myschool";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
+// for AWS
+$aws_server = "13.54.4.168:3306";
+$aws_username = "root";
+$aws_password = "root";
+
+// Create AWSconnection
+$conn = new mysqli($aws_server, $aws_username, $aws_password, $dbname);
+
+
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 echo "Connected successfully";
-?>
