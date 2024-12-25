9c3867890202cad4924040ba21f9df0df78a40a5
pyc <pychang@nkust.edu.tw>
2024-12-25 22:30:00 +0800

    update

--- ch12/ch12_sup/sup02_form.php
+++ ch12/ch12_sup/sup02_form.php
@@ -18,7 +18,7 @@ SELECT * FROM students
     </form>
 
     <?php
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if ($_SERVER["REQUEST_METHOD"] == "POST" && !isset($_POST['insert'])) {
         $query = $_POST['query'];
 
         // Database connection
@@ -55,6 +55,60 @@ SELECT * FROM students
             $result->free();
         } else {
             echo "Error: " . $conn->error;
+        }
+
+        // Close connection
+        $conn->close();
+    }
+    ?>
+
+    <h1>Insert New Student Record</h1>
+    <form method="post" action="">
+        <label for="sno">Student Number:</label><br>
+        <input type="text" id="sno" name="sno"><br><br>
+        <label for="name">Name:</label><br>
+        <input type="text" id="name" name="name"><br><br>
+        <label for="address">Address:</label><br>
+        <input type="text" id="address" name="address"><br><br>
+        <label for="birthday">Birthday:</label><br>
+        <input type="date" id="birthday" name="birthday"><br><br>
+        <label for="username">Username:</label><br>
+        <input type="text" id="username" name="username"><br><br>
+        <label for="password">Password:</label><br>
+        <input type="password" id="password" name="password"><br><br>
+        <input type="submit" value="Insert" name="insert">
+    </form>
+
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['insert'])) {
+        $sno = $_POST['sno'];
+        $name = $_POST['name'];
+        $address = $_POST['address'];
+        $birthday = $_POST['birthday'];
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+
+        // Database connection
+        $servername = "localhost";
+        $db_username = "root";
+        $db_pwd = "";
+        $dbname = "myschool";
+
+        // Create connection
+        $conn = new mysqli($servername, $db_username, $db_pwd, $dbname);
+
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+
+        // Insert query
+        $insert_query = "INSERT INTO students (sno, name, address, birthday, username, password) VALUES ('$sno', '$name', '$address', '$birthday', '$username', '$password')";
+
+        if ($conn->query($insert_query) === TRUE) {
+            echo "New record created successfully";
+        } else {
+            echo "Error: " . $insert_query . "<br>" . $conn->error;
         }
 
         // Close connection
--- ch12/ch12_sup/sup04_form_fun_update.php
+++ ch12/ch12_sup/sup04_form_fun_update.php
@@ -1,4 +1,3 @@
-
 <html lang="en">
 
 <head>
@@ -8,32 +7,70 @@
 
 <body>
     <h1>更新 myschool 資料庫學生資料</h1>
+    
+    <!-- Search Form -->
+    <form method="post" action="">
+        <label for="search_sid">請輸入學號查詢:</label>
+        <input type="text" id="search_sid" name="search_sid" size="6"/>
+        <input type="submit" name="Search" value="查詢">
+    </form>
+    
+    <?php
+    if (isset($_POST["Search"])) {
+        require_once 'sup02_db_connection.php';
+        
+        // 使用函數獲取資料庫連接
+        $conn = getDatabaseConnection();
+        
+        // 建立查詢記錄的SQL指令字串
+        $search_sid = $_POST["search_sid"];
+        $sql = "SELECT * FROM students WHERE sno='$search_sid'";
+        
+        $result = $conn->query($sql);
+        
+        if ($result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+            $sid = $row["sno"];
+            $name = $row["name"];
+            $address = $row["address"]; 
+            $birthday = $row["birthday"];
+            $username = $row["username"];
+            $password = $row["password"];
+        } else {
+            echo "查無此學號的資料。<br/>";
+        }
+        
+        // 關閉連接
+        $conn->close();
+    }
+    ?>
+    
+    <!-- Update Form -->
     <form method="post" action="">
         <table border="1">
             <tr>
                 <td>學號:</td>
-                <td><input type="text" name="sid" size="6"/></td>
+                <td><input type="text" name="sid" size="6" value="<?php echo $sid ?? ''; ?>" readonly/></td>
             </tr>
             <tr>
                 <td>姓名:</td>
-                <td><input type="text" name="sname" size="20"/></td>
+                <td><input type="text" name="name" size="20" value="<?php echo $name ?? ''; ?>"/></td>
             </tr>
             <tr>
-                <td>性別:</td>
-                <td>
-                    <input type="radio" id="male" name="gender" value="M">
-                    <label for="male">男</label>
-                    <input type="radio" id="female" name="gender" value="F">
-                    <label for="female">女</label>
-                </td>
+                <td>地址:</td>
+                <td><input type="text" name="address" size="50" value="<?php echo $address ?? ''; ?>"/></td>
             </tr>
             <tr>
                 <td>生日:</td>
-                <td><input type="date" name="birthday"/></td>
+                <td><input type="date" name="birthday" value="<?php echo $birthday ?? ''; ?>"/></td>
             </tr>
             <tr>
-                <td>電話:</td>
-                <td><input type="text" name="phone" size="20"/></td>
+                <td>用戶名:</td>
+                <td><input type="text" name="username" size="12" value="<?php echo $username ?? ''; ?>"/></td>
+            </tr>
+            <tr>
+                <td>密碼:</td>
+                <td><input type="password" name="password" size="12" value="<?php echo $password ?? ''; ?>"/></td>
             </tr>
         </table>
         <hr>
@@ -46,24 +83,25 @@
         
         // 使用函數獲取資料庫連接
         $conn = getDatabaseConnection();
-
+        
         // 建立更新記錄的SQL指令字串
         $sql = "UPDATE students SET ";
-        $sql .= "sname='".$_POST["sname"]."', ";
-        $sql .= "gender='".$_POST["gender"]."', ";
+        $sql .= "name='".$_POST["name"]."', ";
+        $sql .= "address='".$_POST["address"]."', ";
         $sql .= "birthday='".$_POST["birthday"]."', ";
-        $sql .= "phone='".$_POST["phone"]."' ";
+        $sql .= "username='".$_POST["username"]."', ";
+        $sql .= "password='".$_POST["password"]."' ";
         $sql .= "WHERE sno='".$_POST["sid"]."'";
         
         echo "<b>SQL指令: $sql</b><br/>";
-
+        
         // 執行更新
         if ($conn->query($sql)) {
-            echo "資料庫更新記錄成功, 影響記錄數: " . $conn->affected_rows . "<br/>";
+            echo "資料庫更新記錄成功, 影響記錄數: {$conn->affected_rows}<br/>";
         } else {
-            echo "Error: " . $conn->error;
+            echo "Error: {$conn->error}";
         }
-
+        
         // 關閉連接
         $conn->close();
     }
